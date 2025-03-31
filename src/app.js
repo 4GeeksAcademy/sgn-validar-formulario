f5854727bc5f366d88b4ca5788ce65edea093f0d
Sebastián <BICHOS1506@GMAIL.COM>
2025-03-31 01:53:13 +0000

    formulario validación me falta corregir un error

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+
+
+document.getElementById('myForm').addEventListener('submit', function(event) {  
+ 
+  event.preventDefault();  
+  
+
+  const inputs = this.querySelectorAll('input, select, textarea');  
+  let allValid = true;  
+
+
+  inputs.forEach(input => {  
+      input.classList.add("red-200");  
+  }); 
+
+  inputs.forEach(input => {  
+      if (!input.value.trim()) { 
+          input.classList.add("red-200");
+          allValid = false; 
+      }  
+  });  
+
+ 
+  if (allValid) {  
+      alert('Formulario enviado correctamente.');  
+      
+      this.submit(); 
+  } else {  
+      let mensaje = document.getElementById ('alertaDanger')
+      alert (mensaje); 
+  }  
+});  
+
+
+
